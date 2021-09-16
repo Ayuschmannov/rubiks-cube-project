71e83e0085a803e522958d8e20be839b5942b47c
Ayushman Tiwari <69394281+Ayuschmannov@users.noreply.github.com>
2021-09-16 11:56:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,9 +10,12 @@ for file in glob.glob(path):
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     
     #defining coordinate lists
+    cord = []
+    sorted_cord = []
     final = []
     color = [] 
-    color2 = []
+    color_distinct = []
+    
     # right now, trying on perfect images, afterwards, will try to blur etc
     # def preprocess(img):
     #     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -23,14 +26,14 @@ for file in glob.glob(path):
     #     img_erode = cv2.erode(img_dilate, kernel, iterations=1)
     #     return img_erode
 
-    # defining lower limits
+    # defining lower limits of colors
     lowred = np.array([0,190,20])
     lowyell = np.array([15,160,20])
     lowblu = np.array([100,100,20])
     lowpink = np.array([140,100,20])
     lowgreen = np.array([55,100,20])
 
-    # defining higher limits
+    # defining higher limits of colors
     hired = np.array([10,255,255])
     hiyell = np.array([50,255,255])
     hiblu = np.array([135, 255, 255])
@@ -50,33 +53,48 @@ for file in glob.glob(path):
     cntryell, hierarchyyell = cv2.findContours(maskyell, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     cntrgreen, hierarchygreen = cv2.findContours(maskgreen, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     cntrpink, hierarchypink = cv2.findContours(maskpink, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    
+    cntrall = cntrred + cntrblu + cntryell + cntrgreen + cntrpink  # collection of all contours 
 
-    cntrall = cntrred + cntrblu + cntryell + cntrgreen + cntrpink
+    for contour in cntrall:
+        M = cv2.moments(contour)
+        cx = int(M["m10"] / M["m00"])
+        cy = int(M["m01"] / M["m00"])
+        cord.append([cx, cy])
+#         print("printing coords: ", cord)
 
-    sorted_ctrs = sorted(cntrall, key=lambda ctr: cv2.boundingRect(ctr)[0] + cv2.boundingRect(ctr)[1] * img.shape[1] )
-    for i, ctr in enumerate(sorted_ctrs):
-            x, y, w, h = cv2.boundingRect(ctr)
-            x1 = int(x+(w/2))
-            y1 = int(y+(h/2))
-            color.append((img[y1, x1]).tolist())
+    sort_y = sorted(cord, key=lambda k: k[1])
+    first_row = sort_y[0:3]
+    second_row = sort_y[3:6]
+    third_row = sort_y[6:9]
 
-    k = 0
+    for y1, y2, y3 in first_row, second_row, third_row:
+        sort_x1 = sorted(first_row, key=lambda k: k[0])
+        sort_x2 = sorted(second_row, key=lambda k: k[0])
+        sort_x3 = sorted(third_row, key=lambda k: k[0])
+    sorted_cord = sort_x1 + sort_x2 + sort_x3
+#     print("sorted cord []= ", sorted_cord)
+
+    for i in sorted_cord:
+        sx = i[0]
+        sy = i[1]
+        color.append((img[sy, sx]).tolist())
+
     for x in color:
-            if x not in color2:
-                color2.append(x)
+        if x not in color_distinct:
+            color_distinct.append(x)
 
     for y in color:
-        for z in color2:
+        for z in color_distinct:
             if y == z:
-                final.append(color2.index(z) + 1)
+                final.append(color_distinct.index(z) + 1)
 
 
-    # print(final)
+    # print(final)  #printing the final numbered array
 
     output_path = '<path>'
     completeName = output_path + str(img_number) + '.txt'
     img_number += 1
-    # print(completeName)
     # cv2.imshow("color detection", img)
     f = open(completeName, "w+")
     for a in range(0,9):
@@ -88,6 +106,6 @@ for file in glob.glob(path):
 
 
     # Wait Key
-    cv2.waitKey(0)
+#     cv2.waitKey(0)
     # Destroy window
-    cv2.destroyAllWindows()
+#     cv2.destroyAllWindows()
